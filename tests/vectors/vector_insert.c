5fb13a512e5345df8d06504157a2b7a8be670cdd
= <jhowell45@googlemail.com>
2024-10-21 13:45:54 +0100

    Updating initial check to make sure they're all zero.

--- tests/vectors/vector_insert.c
+++ tests/vectors/vector_insert.c
@@ -1,19 +1,23 @@
 #include "unity.h"
 #include "vector.h"
 
+#define VECTOR_SIZE 3
+
 void setUp(void) {}
 
 void tearDown(void) {}
 
 static void test_initial_value_is_zero_for_created_vector(void) {
-  vector_t *vec = new_vector(3);
-  TEST_ASSERT_EQUAL_DOUBLE(vector_get(vec, 0), 0);
+  vector_t *vec = new_vector(VECTOR_SIZE);
+  for (int i = 0; i < VECTOR_SIZE; i++) {
+    TEST_ASSERT_EQUAL_DOUBLE(vector_get(vec, i), 0);
+  }
   free_vector(vec);
 }
 
 static void test_insert_value_to_vector(void) {
   double val = 5.0;
-  vector_t *vec = new_vector(3);
+  vector_t *vec = new_vector(VECTOR_SIZE);
   vector_insert(vec, 0, val);
   TEST_ASSERT_EQUAL_DOUBLE(vector_get(vec, 0), val);
   free_vector(vec);
