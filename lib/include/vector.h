73f1f497b88fc2081a84f4ce4830f5eaa8381199
= <jhowell45@googlemail.com>
2024-10-21 01:15:04 +0100

    function for displaying a vector.

--- lib/include/vector.h
+++ lib/include/vector.h
@@ -12,6 +12,7 @@ typedef struct {
 vector_t *new_vector(size_t size);
 vector_t *random_vector(size_t size);
 void free_vector(vector_t *vec);
+void display_vector(vector_t *vec);
 
 bool vector_insert(vector_t *vec, size_t index, double value);
 
