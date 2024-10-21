a553bbb76d1590c8c080537650cd243c15eaad0f
= <jhowell45@googlemail.com>
2024-10-21 01:14:46 +0100

    function for creating a vector with random values.

--- lib/include/vector.h
+++ lib/include/vector.h
@@ -10,6 +10,7 @@ typedef struct {
 } vector_t;
 
 vector_t *new_vector(size_t size);
+vector_t *random_vector(size_t size);
 void free_vector(vector_t *vec);
 
 bool vector_insert(vector_t *vec, size_t index, double value);
