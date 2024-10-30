8bc884409502d9532db63ae435299ef691a6a100
= <jhowell45@googlemail.com>
2024-10-30 00:41:43 +0000

    Created basic header for the matrix impl.

--- lib/include/matrix.h
+++ lib/include/matrix.h
@@ -9,4 +9,14 @@ typedef struct {
     size_t columns;
 } matrix_t;
 
+matrix_t *new_matrix(size_t rows, size_t columns);
+matrix_t *new_matrix_with_default(size_t rows, size_t columns, double default_value);
+matrix_t *random_matrix(size_t rows, size_t columns);
+void free_matrix(matrix_t *m);
+void display_matrix(matrix_t *m);
+
+double matrix_get(matrix_t *m, size_t x, size_t y);
+
+bool matrix_insert(matrix_t *m, size_t x, size_t y, double value);
+
 #endif
