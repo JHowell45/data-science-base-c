ccea68be2ce080c9aa610f722dd119b029bdb2fc
= <jhowell45@googlemail.com>
2024-10-30 01:35:36 +0000

    starting on impl for vector/matrix multi.

--- lib/src/matrix.c
+++ lib/src/matrix.c
@@ -1,4 +1,5 @@
 #include "matrix.h"
+#include "vector.h"
 
 #include <stdlib.h>
 
@@ -12,12 +13,19 @@ matrix_t *new_matrix(size_t rows, size_t columns) {
 matrix_t *new_matrix_with_default(size_t rows, size_t columns,
                                   double default_value) {}
 matrix_t *random_matrix(size_t rows, size_t columns) {}
-void free_matrix(matrix_t *m) {}
+void free_matrix(matrix_t *m) {
+  if (m->items != NULL) {
+    free(m->items);
+  }
+  free(m);
+}
 void display_matrix(matrix_t *m) {}
 
 double matrix_get(matrix_t *m, size_t x, size_t y) {}
 
 bool matrix_insert(matrix_t *m, size_t x, size_t y, double value) {}
 
-matrix_t *matrix_dot_vector(matrix_t *m, vector_t *v) {}
+matrix_t *matrix_dot_vector(matrix_t *m, vector_t *v) {
+  vector_t *results = new_vector(v->size);
+}
 matrix_t *matrix_dot_matrix(matrix_t *m1, matrix_t *m2) {}
